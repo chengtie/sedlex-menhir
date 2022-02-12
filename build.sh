7884457a17e816f00dafe85dc8f1c5306dadee86
chengtie <chengtie@gmail.com>
2022-02-12 10:30:51 +0100

    removed build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-ocamlc syntax.ml
-# menhir
-menhir --ocamlc 'ocamlfind ocamlc -thread -package sedlex -package menhirLib' --explain --infer parser.mly
-ocamlc -c parser.mli
-ocamlc -c parser.ml
-# sedlex
-ocamlfind ocamlc -c -package sedlex.ppx lexer.ml
-ocamlfind ocamlc -o calc -linkpkg -package sedlex -package menhirLib syntax.cmo lexer.cmo parser.cmo calc.ml
